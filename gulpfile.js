8e3521afc5697f3455487db7187850e48d42b60a
Azubkovas2001 <109983488+Azubkovas2001@users.noreply.github.com>
2022-09-18 14:46:21 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+
+var tabs = document.querySelectorAll(".tab")
+var buttons = document.querySelectorAll("h2")
+
+function showTab(event, id){
+    for(var x of tabs){
+        x.style.display = "none"
+        x.style.opacity = "0"
+    }
+    document.getElementById(id).style.display = "block"
+    setTimeout(function(){
+    document.getElementById(id).style.opacity = "1"
+    })
+
+    for(var x of buttons){
+        x.classList.remove("selected")
+    }
+}
